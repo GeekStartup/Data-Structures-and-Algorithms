1f9c33d432f482ca0d1900b10584d3c0ea9ad1d9
Ashish Nayak <ashishn.128@ext.zeta.tech>
2024-04-11 17:21:05 +0530

    GCD/HCF using Euclid algorithm

--- src/GCDorHCF.java
+++ src/GCDorHCF.java
@@ -12,16 +12,42 @@ public class GCDorHCF {
     System.out.println(
         "GCD/HCF of " + num1 + " and " + num2 + " using brute force is: " + bruteForceGCDorHCF(num1,
             num2));
+    System.out.println(
+        "GCD/HCF of " + num1 + " and " + num2 + " using Euclid algorithm is: " + euclidGCDorHCF(
+            num1,
+            num2));
   }
 
+  //Brute Force
   static int bruteForceGCDorHCF(int num1, int num2) {
+    long startTime = System.currentTimeMillis();
+    long endTime;
     int min = Math.min(num1, num2);
     for (int i = min; i >= 1; i--) {
       if (num1 % i == 0 && num2 % i == 0) {
+        endTime = System.currentTimeMillis();
+        System.out.println("Time taken by brute force : " + (endTime - startTime) + " ms");
         return i;
       }
     }
+    endTime = System.currentTimeMillis();
+    System.out.println("Time taken by brute force : " + (endTime - startTime) + " ms");
     return 1;
+  }
+
+  //Euclid
+  static int euclidGCDorHCF(int num1, int num2) {
+    long startTime = System.currentTimeMillis();
+    while (num1 != num2) {
+      if (num1 > num2) {
+        num1 = num1 - num2;
+      } else {
+        num2 = num2 - num1;
+      }
+    }
+    long endTime = System.currentTimeMillis();
+    System.out.println("Time taken by Euclid algorithm : " + (endTime - startTime) + " ms");
+    return num1;
   }
 
 }
