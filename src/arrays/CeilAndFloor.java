f5a7fb7cf382ad347823b10dcba68b36c6f24e34
Ashish Nayak <ashishn.128@ext.zeta.tech>
2024-04-14 13:40:39 +0530

    Ceil and Floor

--- src/arrays/CeilAndFloor.java
+++ src/arrays/CeilAndFloor.java
@@ -3,6 +3,7 @@ package arrays;
 import java.util.Arrays;
 import java.util.Scanner;
 
+
 public class CeilAndFloor {
 
   public static void main(String[] args) {
@@ -11,15 +12,43 @@ public class CeilAndFloor {
     System.out.println(
         "Enter a number to find the ceil and floor from " + Arrays.toString(arr) + ": ");
     int key = scanner.nextInt();
-    System.out.println("Ceil of " + key + " is: " + ceil(key));
-    System.out.println("Floor of " + key + " is: " + floor(key));
+    System.out.println("Ceil of " + key + " is: " + ceil(key, arr));
+    System.out.println("Floor of " + key + " is: " + floor(key, arr));
   }
 
-  static int ceil(int key) {
+  static int ceil(int key, int[] arr) {
+    int low = 0, high = arr.length - 1, mid;
+    while (low <= high) {
+      mid = (low + high) / 2;
+      if (key == arr[mid]) {
+        return arr[mid];
+      } else if (key < arr[mid]) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+    if (low < arr.length) {
+      return arr[low];
+    }
     return -1;
   }
 
-  static int floor(int key) {
+  static int floor(int key, int[] arr) {
+    int low = 0, high = arr.length - 1, mid;
+    while (low <= high) {
+      mid = (low + high) / 2;
+      if (key == arr[mid]) {
+        return arr[mid];
+      } else if (key < arr[mid]) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+    if (high >= 0) {
+      return arr[high];
+    }
     return -1;
   }
 
