cc0f477d0f097d3b0c07aaba290ef947fee9fc1f
Ashish Nayak <ashishn.128@ext.zeta.tech>
2024-04-19 07:37:21 +0530

    Search in Rotated Sorted Array

--- src/arrays/SearchInRotatedSortedArray.java
+++ src/arrays/SearchInRotatedSortedArray.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class SearchInRotatedSortedArray {
 
   public static void main(String[] args) {
-    int[] nums = {4, 5, 6, 7, 0, 1, 2};
+    int[] nums = {3,1};
     Scanner scanner = new Scanner(System.in);
     System.out.println(
         "Enter a number to find in RotatedSortedArray " + Arrays.toString(nums) + ": ");
@@ -21,12 +21,14 @@ public class SearchInRotatedSortedArray {
       mid = (low + high) / 2;
       if (target == nums[mid]) {
         return mid;
-      } else if (nums[low] < nums[mid]) {
+        //left sorted portion
+      } else if (nums[low] <= nums[mid]) {
         if (target >= nums[low] && target < nums[mid]) {
           high = mid - 1;
         } else {
           low = mid + 1;
         }
+        //right sorted portion
       } else {
         if (target > nums[mid] && target <= nums[high]) {
           low = mid + 1;
